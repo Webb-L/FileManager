3ccff980394f69d213be4142c4f8fadc5af40bbd
Admin <admin@admin.com>
2023-12-19 18:34:23 +0800

    style:

--- composeApp/src/commonMain/kotlin/app/filemanager/data/file/FileExtensions.kt
+++ composeApp/src/commonMain/kotlin/app/filemanager/data/file/FileExtensions.kt
@@ -1,5 +1,7 @@
 package app.filemanager.data.file
 
+// https://www.iana.org/assignments/media-types/media-types.xhtm
+// https://fileinfo.com/filetypes/
 object FileExtensions {
     val Images = listOf("jpeg", "png", "gif", "bmp", "webp", "midi")
     val Audios = listOf("mp3", "wav", "acc", "ogg", "flac", "midi")
