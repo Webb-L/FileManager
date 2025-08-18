f87ff553dcdf3ef5277f1e8e4ff949df8c7c0df2
webb <822028533@qq.com>
2025-08-18 23:57:52 +0800

    style

--- composeApp/src/commonMain/kotlin/app/filemanager/ui/screen/file/FileShareScreen.kt
+++ composeApp/src/commonMain/kotlin/app/filemanager/ui/screen/file/FileShareScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.material3.*
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.runtime.*
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
